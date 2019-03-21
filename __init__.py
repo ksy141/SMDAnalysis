170f4d4ae8ac036b06a7ddd9c17581b4052306f2
ksy141 <kkssy141@gmail.com>
2019-03-21 17:55:52 -0500

    simple change

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
 from .common.distance import Distance
+from .common.com_distance import COMDistance
 from .common.pbc import PBC
 from .common.traj_to_numpy import TrajectoryToNumpy
 from .common.units import *
 from .common.wholemolecules import Wholemolecules
 from .compute.packing_defects import PackingDefects
-
 # THIS WILL WORK IF I DO
 # from SMDAnalysis import *
 __all__ = ['common', 'compute']
