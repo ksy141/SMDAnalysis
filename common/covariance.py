d14feb05441c4fd1d9cbe483bc0ca8115c56f442
siyoungkim <kkssy141@gmail.com>
2019-04-20 12:05:42 -0500

    covar

--- common/covariance.py
+++ common/covariance.py
@@ -1,37 +1,39 @@
 import numpy as np
+from decimal import Decimal
 
 class Covariance:
     def __init__(self):
         pass
 
     def calculate(self, lag=0, features=None):
+        print("==================================")
+        print("start covariance calculation")
         nfeatures = len(features)
         for feature in features:
             assert feature.shape == features[0].shape, "different size"
         features = np.array(features)
         
-        dt = features[0][1][0] - features[0][0][0]
+        dt = features[0][2][0] - features[0][1][0]
         nf = len(features[0][:,0])
         print("nfeatures: ", nfeatures)
         print("dt: %.2f ns" %dt)
+        print("lag: %.2f ns" %lag)
         print("nframes: ", nf)
 
         if lag != 0:
             assert dt < lag, "dt > lag"
 
-        if lag % dt < 0.0001:
+        if Decimal('%f' %lag) % Decimal('%f' %dt) < 0.001:
             print("good lag time")
         else:
             print("bad lag time")
-            lag = (lag // dt) * dt
+            lag = (Decimal('%f' %lag) // Decimal('%f' %dt)) * dt
             print("adjusting lag time to %.2f ns" %lag)
 
-        df = int(lag//dt)
+        df = int(Decimal('%f' %lag)//Decimal('%f' %dt))
         
         n = 0
         covar = np.zeros((nfeatures, nfeatures))
-        print("==================================")
-        print("start covariance calculation")
         while (n + df) < nf:
             X = features[:, n, 1]
             Y = features[:, n + df, 1]
