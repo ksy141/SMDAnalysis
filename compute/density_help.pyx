83bd180e1f148411351ab085913d52f74a5e6a94
ksy141 <kkssy141@gmail.com>
2019-11-17 19:27:47 -0600

    density_help

--- compute/density_help.pyx
+++ compute/density_help.pyx
@@ -1,16 +1,15 @@
 import numpy as np
-cimport numpy as cnp
+cimport numpy as np
 import cython
 
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 
-def density_help(cnp.ndarray[long, ndim=1] indices,
-                 cnp.ndarray[double, ndim=1] masses,
-                 cnp.ndarray[double, ndim=1] density):
+def density_help(np.ndarray[np.int64_t,   ndim=1] indices,
+                 np.ndarray[np.float64_t, ndim=1] masses,
+                 np.ndarray[np.float64_t, ndim=1] density):
     
     cdef int i = 0
-    cdef long index
     cdef int s = masses.shape[0]
     
     for i in range(0, s):
--- compute/density_help_setup.py
+++ compute/density_help_setup.py
@@ -2,11 +2,13 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
+# python density_help_setup.py build_ext --inplace
+
 setup(
-        ext_modules = cythonize("*.pyx", 
-                                annotate=True,
-                                language_level = "3"),
-        include_dirs=[numpy.get_include()]
+    ext_modules = cythonize("*.pyx", 
+                            annotate = False,
+                            language_level = "3"),
+    include_dirs=[numpy.get_include()]
 )
 
 
